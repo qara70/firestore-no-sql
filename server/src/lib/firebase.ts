41fbe32b467a800dcf7f7f814865d1724366af2c
qara70 <entsuka70@gmail.com>
2021-12-20 08:20:11 +0900

    add: firebase-admin初期化・firestoreの初期化

--- server/src/lib/firebase.ts
+++ server/src/lib/firebase.ts
@@ -1,6 +1,7 @@
-import firebase from 'firebase/compat/app';
-import { getFirestore } from 'firebase/firestore';
-import { firebaseConfig } from './firebase-config';
+import * as admin from 'firebase-admin';
+import { ServiceAccount } from 'firebase-admin';
+import * as serviceAccount from './praha-firestore-example-firebase.json';
 
-export const firebaseApp = firebase.initializeApp(firebaseConfig);
-export const db = getFirestore();
+export const firebase = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as ServiceAccount),
+});
