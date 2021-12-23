df6a94e66a5eb84fc6ade58bf8e0b7dcafc49ef9
qara70 <entsuka70@gmail.com>
2021-12-23 22:33:27 +0900

    fix: firebase config修正

--- server/src/lib/firebase-config.ts
+++ server/src/lib/firebase-config.ts
@@ -1,4 +1,6 @@
-export const firebaseConfig = {
+import { FirebaseOptions } from 'firebase/app';
+
+export const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
   projectId: process.env.PROJECT_ID,
